4f891dd427479b247390a2b91c98c0d841d96999
fransiskatesting <testingfransiska@gmail.com>
2026-08-21 12:53:57 +0700

    Refactor Cypress config for improved readability

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 30000,
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx}',
+    setupNodeEvents(on, config) {},
   },
-});
+})
